8d0e8a86651450978057458fca25235ea5757d4e
billyxu <billyxuxyxbb@gmail.com>
2019-04-13 03:25:20 +0000

    用户个人页面原型

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,7 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
 Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
+
+//个人信息
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);  //显示用户个人信息
