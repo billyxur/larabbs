4db942b2a03c303960429569f8defa9d8d38fa81
billyxu <billyxuxyxbb@gmail.com>
2019-04-13 08:34:32 +0000

    编辑个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -11,5 +12,25 @@ class UsersController extends Controller
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(UserRequest $request, User $user)
+    {
+//        $data = $request->all();
+//        if($user->update($data)){
+//            return redirect()->route('users.show', $user->id)->with('success', '个人信息更新成功');
+//        }
+//
+//        return redirect()->route('users.show', $user->id)->with('danger', '个人信息更新失败');
+
+
+        /**使用了表单验证，所有表单提交数据都经过UserRequest验证，如果更新失败会直接抛出exception,并重定向至上一个页面，附带验证失败的信息。**/
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人信息更新成功');
     }
 }
