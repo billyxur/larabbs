5de3d4d6f47bdacbf533555176f0f54303b8e3c5
billyxu <billyxuxyxbb@gmail.com>
2019-06-19 05:40:50 +0000

    翻譯 URL 標示

--- routes/web.php
+++ routes/web.php
@@ -45,8 +45,10 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 
 //个人信息
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);  //显示用户个人信息
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
