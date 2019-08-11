08fe8d3551788d94d0b9915fd03b2d95ad6f4cc4
billyxu <billyxuxyxbb@gmail.com>
2019-08-11 09:13:13 +0000

    消息通知有新回复

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,29 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
+use Auth;
 
 class User extends Authenticatable implements MustVerifyEmailContract
 {
-    use Notifiable,MustVerifyEmailTrait;
+    use MustVerifyEmailTrait;
+
+    use Notifiable {
+        notify as protected laraveNotify;
+    }
+
+    public function notify($instance)
+    {
+        // 如果通知的人是当前的用户， 就不必通知了
+        if($this->id == Auth::id()){
+            return ;
+        }
+
+        // 只有数据库类型通知才需要提醒， 直接发送EMAIL 或者其他的pass
+        if(method_exists($instance, 'toDatabase')) {
+            $this->increment('notification_count');
+        }
+
+        $this->laraveNotify($instance);    }
 
     /**
      * The attributes that are mass assignable.
